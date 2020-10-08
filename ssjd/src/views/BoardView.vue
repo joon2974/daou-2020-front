3ff6b2d23e8995873b4c4a94ac0fecc88c97a5f3
JeongkeonLee <vonjobi2@gmail.com>
2020-10-08 17:35:18 +0900

    feat(post) : Add FindAllByLanguage
    언어별 리스트 조회 기능 추가하였습니다.

--- ssjd/src/views/BoardView.vue
+++ ssjd/src/views/BoardView.vue
@@ -13,9 +13,20 @@
               class="shrink"
               outline
               centered-input
+              v-on:keypup="onkeyup"
+              v-on:submit.prevent="onSearch"
             >
             </v-text-field>
-            <v-btn depressed center color="blue white--text">검색</v-btn>
+            <v-btn
+              depressed
+              center
+              color="blue white--text"
+              v-show="keyword.length"
+              v-on:click="onReset"
+              type="reset"
+              class="btn-reset"
+              >검색</v-btn
+            >
           </v-row>
         </v-flex>
         <v-flex xs12>
@@ -29,31 +40,42 @@
                         label="프로그래머스"
                         color="primary"
                         value="programmers"
+                        @click="chageSite('programmers')"
                       ></v-radio>
                       <v-radio
                         label="백준"
                         color="primary"
                         value="backjun"
+                        name="backjun"
+                        @click="chageSite('backjun')"
                       ></v-radio>
                     </v-radio-group>
                   </v-flex>
                   <v-flex xs10 sm5 md5>
                     <v-radio-group v-model="ex8" column>
-                      <v-radio label="C++" color="error" value="C++"></v-radio>
+                      <v-radio
+                        label="C++"
+                        color="error"
+                        value="C++"
+                        @click="chageLanguage('C++')"
+                      ></v-radio>
                       <v-radio
                         label="Java"
                         color="error"
                         value="Java"
+                        @click="chageLanguage('Java')"
                       ></v-radio>
                       <v-radio
                         label="Javascript"
                         color="error"
                         value="Javascript"
+                        @click="chageLanguage('Javascript')"
                       ></v-radio>
                       <v-radio
                         label="Python"
                         color="error"
                         value="Python"
+                        @click="chageLanguage('Python')"
                       ></v-radio>
                     </v-radio-group>
                   </v-flex>
@@ -70,7 +92,7 @@
         :post="post"
       >
       </post-card>
-      <infinite-loading @infinite="infiniteHandler" spinner="waveDots">
+      <infinite-loading @infinite="infiniteHandler" spinner="spiral">
         <div
           slot="no-more"
           style="color: rgb(102, 102, 102); font-size: 14px; padding: 10px 0px;"
@@ -98,9 +120,15 @@ export default {
       posts: [],
       limit: 0,
       language: "",
+      beforeLanguage: "",
       site: "",
+      beforeSite: "",
+      keyword: "",
       srcs: null,
       item: 0,
+      ex7: "primary",
+      ex8: "red",
+      api: [`http://localhost:3000/api/posts?pageNum=0`],
     };
   },
   name: "Topic",
@@ -120,7 +148,10 @@ export default {
   methods: {
     infiniteHandler($state) {
       const EACH_LEN = 6;
-
+      let language = this.language;
+      let site = this.site;
+      console.log(language);
+      console.log(site);
       fetch("http://localhost:3000/api/posts?pageNum=" + this.limit, {
         method: "get",
       })
@@ -133,7 +164,6 @@ export default {
               this.posts = this.posts.concat(data);
               $state.loaded();
               this.limit += 1;
-              console.log("after", this.posts.length, this.limit);
 
               if (data.length / EACH_LEN < 1) {
                 $state.complete();
@@ -147,11 +177,41 @@ export default {
           console.error(err);
         });
     },
+    onSearch(e) {},
+    onkeyup() {
+      if (!this.keyword.length) this.onReset();
+    },
+    onReset() {
+      this.keyword = "";
+      debugger;
+    },
+    chageSite(value) {
+      this.site = value;
+    },
+    chageLanguage(value) {
+      console.log(this.site);
+      console.log("??");
+      this.language = value;
+      this.posts = [];
+      if (this.site == "" || this.site == null) {
+        console.log("실행");
+        axios
+          .get(
+            `http://localhost:3000/api/posts/language/` + value + `?pageNum=0`,
+            headers
+          )
+          .then((data) => {
+            this.posts = [...data.data];
+          });
+      } else {
+        console.log("안실행" + this.site);
+      }
+    },
   },
 };
 </script>
 <style scoped>
-/deep/ .v-text-field {
+.v-text-field {
   width: 400px;
 }
 </style>

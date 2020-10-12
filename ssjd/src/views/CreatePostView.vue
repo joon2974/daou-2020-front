8bae2133cfce2a3f69536b3a81708e093b0c45ba
Youngjun Hwang <joon2974@daou.co.kr>
2020-10-12 11:00:33 +0900

    feat(member): profile photo, csrf protect
    프로필 사진 코드 수정
    csrf 방어를 위한 쿠키 추가 코드 일부 구현

--- ssjd/src/views/CreatePostView.vue
+++ ssjd/src/views/CreatePostView.vue
@@ -2,19 +2,19 @@
   <v-container fluid>
     <v-row>
       <v-col cols="6">
-        <v-sheet elevation="12" class="mx-2 my-6 py-2" rounded>
+        <v-sheet elevation="12" class="mx-2 my-6" rounded>
           <post-info @postInfoEmit="changePostInfo"></post-info>
         </v-sheet>
       </v-col>
       <v-col cols="6">
-        <v-sheet elevation="12" class="mx-2 my-6 py-2" rounded>
+        <v-sheet elevation="12" class="mx-2 my-6" rounded>
           <problem-info @problemInfoEmit="changeProblemInfo"></problem-info>
         </v-sheet>
       </v-col>
     </v-row>
     <v-row>
       <v-col cols="6">
-        <v-sheet elevation="12" class="mx-2 my-6 py-2" rounded>
+        <v-sheet elevation="12" class="mx-2 my-6" rounded>
           <solution-code-placeholder
             @requestCodeHighlight="makeCodePretty"
             @solutionCodeInfoEmit="changeSolutionInfo"
@@ -22,14 +22,14 @@
         </v-sheet>
       </v-col>
       <v-col cols="6">
-        <v-sheet elevation="12" class="mx-2 my-6 py-2" rounded>
+        <v-sheet elevation="12" class="mx-2 my-6" rounded>
           <highlighted-code-placeholder
             :solutionCode="solutionCode"
             :selectedLanguage="selectedLanguage"
           >
           </highlighted-code-placeholder>
           <v-row align="center" justify="end">
-            <v-btn tile color="success" class="mr-8" @click="postPost"
+            <v-btn tile color="success" class="mr-8 mb-4" @click="postPost"
               >게시글 올리기</v-btn
             >
           </v-row>
@@ -43,6 +43,7 @@
 import axios from "axios";
 import { mapState } from "vuex";
 import { httpInfos } from "../../secretStrings";
+import { generateCsrfToken } from "../../secretStrings";
 import PostInfo from "../components/CreatePostComponents/PostInfo";
 import ProblemInfo from "../components/CreatePostComponents/ProblemInfo";
 import SolutionCodePlaceholder from "../components/CreatePostComponents/SolutionCodePlaceholder";
@@ -115,7 +116,13 @@ export default {
         return;
       }
 
-      return axios
+      const csrfToken = generateCsrfToken().replace(/=/gi, "");
+      this.$cookies.set("CSRF_TOKEN", csrfToken);
+
+      axios.defaults.headers.common["CSRF_TOKEN"] = csrfToken;
+      axios.defaults.headers.common["CSRF_TOKEN_IN_COOKIE"] = this.$cookies.get("CSRF_TOKEN");
+
+      axios
         .post(
           `${httpInfos.resourceHost}/posts`,
           this.requestDto,
@@ -129,6 +136,10 @@ export default {
           alert("게시글 작성에 실패했어요");
           console.log(e);
         });
+
+      delete axios.defaults.headers.common["CSRF_TOKEN"];
+      delete axios.defaults.headers.common["CSRF_TOKEN_IN_COOKIE"];
+      this.$cookies.remove("CSRF_TOKEN");
     },
   },
 };
